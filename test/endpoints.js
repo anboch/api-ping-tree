b98994890392103762608ce20eb5752a7089e5e6
anboch <a.g.bochka@gmail.com>
2021-08-24 17:00:16 +0300

    test: POST /api/targets/:id

--- test/endpoints.js
+++ test/endpoints.js
@@ -121,6 +121,39 @@ test.serial.cb('GET /api/targets/:id : can not get a target by nonexist id', fun
   }).end(JSON.stringify(testData.target1))
 })
 
+test.serial.cb('POST /api/targets/:id : update target by id', function (t) {
+  const urlSetTarget = '/api/targets'
+  const urlUpdateTargetById = `/api/target/${testData.target1.id}`
+  const urlGetTargetById = `/api/target/${testData.target2.id}`
+  servertest(server(), urlSetTarget, { encoding: 'json', method: 'POST' }, function (err, res) {
+    t.falsy(err, 'no error')
+    servertest(server(), urlUpdateTargetById, { encoding: 'json', method: 'POST' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.is(res.statusCode, 200, 'correct statusCode')
+      t.is(res.body.message, 'UPDATED', 'correct message')
+      servertest(server(), urlGetTargetById, { encoding: 'json' }, function (err, res) {
+        t.falsy(err, 'no error')
+        t.is(res.statusCode, 200, 'correct statusCode')
+        t.is(res.body.id, testData.target2.id, 'correct target Id')
+        t.end()
+      })
+    }).end(JSON.stringify(testData.target2))
+  }).end(JSON.stringify(testData.target1))
+})
+
+test.serial.cb('POST /api/targets/:id : can not update target by nonexist id', function (t) {
+  const urlSetTarget = '/api/targets'
+  const urlGetTarget = `/api/target/${Number(testData.target1.id) + 1}`
+  servertest(server(), urlSetTarget, { encoding: 'json', method: 'POST' }, function (err, res) {
+    t.falsy(err, 'no error')
+    servertest(server(), urlGetTarget, { encoding: 'json', method: 'POST' }, function (err, res) {
+      t.falsy(err, 'no error')
+      t.is(res.body.message, 'Target not found', 'correct message')
+      t.end()
+    }).end(JSON.stringify(testData.target2))
+  }).end(JSON.stringify(testData.target1))
+})
+
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
   servertest(server(), url, { encoding: 'json' }, function (err, res) {
